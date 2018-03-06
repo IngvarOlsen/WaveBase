406338c1f7c6ddd470b8810763863197f0153ffe
Nereus <ingvarolsen57@gmail.com>
2018-03-06 02:14:50 +0100

    Add ease scrolling and fixed navbar active classes

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,6 +1,62 @@
+
+//Ease scrolling
+function scrollNav() {
+  $('.nav a').click(function(){ 
+    console.log("Nav button clicked"); 
+    //Toggle Class
+    $(".active").removeClass("active");      
+    $(this).closest('li').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 0
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
+
+
+// Cache selectors
+var topMenu = $("#top-menu"),
+    topMenuHeight = topMenu.outerHeight()+100,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   // Set/remove active class
+   menuItems
+     .parent().removeClass("active")
+     .end().filter("[href='#"+id+"']").parent().addClass("active");
+})
+
+
+
 //Scroll reveal
 window.sr = ScrollReveal();
 sr.reveal('.reveal', { duration: 2000 });
+
+
+
 
 (function() {
 
